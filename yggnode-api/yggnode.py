7c64d5999575281b3dc23343b6a25ecb12d96754
LimeDrive <limecat@limedrive.eu>
2021-05-26 14:06:30 +0200

    update

--- yggnode-api/yggnode.py
+++ yggnode-api/yggnode.py
@@ -2,7 +2,7 @@
 import os
 import re
 import time
-
+import humanize
 import yaml
 from flask import Flask, request, send_file, Response, render_template
 from torrentool.api import Torrent
@@ -13,127 +13,128 @@ USER_PASSKEY = "ijnXPgYNat3VMnCsqofjUsU5zePmZr9C"
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     with open("config/annexes.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)        
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     server = f'{str(cfg["node"]["protocol"])}://{str(cfg["node"]["ipAdress"])}:{str(cfg["node"]["port"])}'
     name = f'{str(cfg["node"]["ipAdress"])}'
-    
-    return server + name
+    return render_template('index.html', server=server, name=name)
 
-    # return "USE : <br> \
-    #        /download?id={torrent_id}&passkey={your_passkey}<br> \
-    #        /rss?id={category id}&passkey={your_passkey}<br><br> \
-    #        Categories List available <a href=" + str(serverConfiguration["node"]["protocol"])\
-    #        + "://" + str(serverConfiguration["node"]["ipAdress"]) + ":" + str(serverConfiguration["node"]["port"]) + "/links><strong>Here</strong></a>"+ \
-    #        "<br><br><a href=" + str(serverConfiguration["node"]["protocol"]) + "://" + str(serverConfiguration["node"]["ipAdress"]) + ":"\
-    #                          + str(serverConfiguration["node"]["port"]) + "/status>Server last time resynchronization</a>"
+# write torrents and send file on request.
 
 
 @app.route('/download', methods=['GET'])
 def generatingTorrent():
     remoteTempTorrent()
-    if not (os.path.isfile(os.getcwd() + "/blackhole/torrents/" + request.args.get("id") + ".torrent")):
+    if not os.path.exists(f'blackhole/torrents/{request.args.get("id")}.torrent'):
         return "torrent unavailable"
-    # grab torrent file matching id provided
-    my_torrent = Torrent.from_file(os.getcwd() + "/blackhole/torrents/" + request.args.get("id") + ".torrent")
+    my_torrent = Torrent.from_file(f'blackhole/torrents/{request.args.get("id")}.torrent')
     # changing passkey for one transmitted as parameter by user
-    newUrlTracker = re.sub("[a-zA-Z0-9]{32}", request.args.get("passkey"), ((my_torrent.announce_urls[0])[0]))
+    newUrlTracker = re.sub(
+        "[a-zA-Z0-9]{32}", request.args.get("passkey"), ((my_torrent.announce_urls[0])[0]))
     my_torrent.announce_urls = newUrlTracker
     # write it in temp dir for more clarity
-    my_torrent.to_file(os.getcwd() + "/blackhole/torrents/tmp/" + request.args.get("id") + request.args.get("passkey") + ".torrent")
+    my_torrent.to_file(f'blackhole/torrents/tmp/{request.args.get("id")}.{request.args.get("passkey")}.torrent')
     # send torrent file
-    return send_file(os.getcwd() + "/blackhole/torrents/tmp/" + request.args.get("id") + request.args.get("passkey") + ".torrent",
+    return send_file(f'blackhole/torrents/tmp/{request.args.get("id")}.{request.args.get("passkey")}.torrent',
                      as_attachment=True,
                      attachment_filename=(my_torrent.name + ".torrent"),
                      mimetype='application/x-bittorrent')
 
+# Rss Feed route
+
 
 @app.route('/rss', methods=['GET'])
 def generatingRSS():
-    remoteTempTorrent()
-    # check if category id is provided and valid
     if request.args.get("id") is None or int(request.args.get("id")) < 2139 \
             or int(request.args.get("id")) == 2146 \
             or int(request.args.get("id")) > 2187:
         return "bad category requested"
-    # check if passkey with valid format is provided
-    if request.args.get("passkey") is None:
+    if request.args.get("passkey") is None or len(request.args.get("passkey")) != 32:
         return "passkey not provided : please send one as parameter 'passkey'"
-
-    if not (os.path.isfile(os.getcwd() + "/blackhole/rss/" + request.args.get("id") + ".xml")):
+    if not os.path.exists(f'blackhole/rss/{request.args.get("id")}.xml'):
         return "rss file unavailable for this category at the moment"
-
-    # opens last updated rss file corresponding to the category called
-    rssFile = open(os.getcwd() + "/blackhole/rss/" + request.args.get("id") + ".xml", "r")
-    txt = rssFile.read()
-    rssFile.close()
-    # create a temp rss generated file with both category and passkey as name to avoid potential simultaneous access
-    # replace original passkey by the one provided by the user
-    txt = re.sub("passkey=[a-zA-Z0-9]{32}", "passkey=" + request.args.get("passkey"), txt)
+    with open(f'blackhole/rss/{request.args.get("id")}.xml', "r") as rssFile:
+        txt = re.sub(
+            "passkey=[a-zA-Z0-9]{32}", f'passkey={request.args.get("passkey")}', rssFile.read())
     return Response(txt, mimetype='text/xml')
+
+# Clear temp files.
 
 
 def remoteTempTorrent():
     now = time.time()
     # browses all files and delete every having more than 5 secs of existence
-    for torrentFile in os.listdir(os.getcwd() + "/blackhole/torrents/tmp/"):
-        if os.stat(os.getcwd() + "/blackhole/torrents/tmp/" + torrentFile).st_mtime < now:
-            os.remove(os.getcwd() + "/blackhole/torrents/tmp/" + torrentFile)
+    for torrentFile in os.listdir("blackhole/torrents/tmp/"):
+        if os.stat(f"blackhole/torrents/tmp/{torrentFile}").st_mtime < now:
+            os.remove(f"blackhole/torrents/tmp/{torrentFile}")
+
+# Links generators route.
+
 
 @app.route('/links', methods=['GET'])
 def generateLinks():
-    
+    # Config file
     with open("config/annexes.yml", 'r') as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-        
+    # Set variables
     server = f'{str(cfg["node"]["protocol"])}://{str(cfg["node"]["ipAdress"])}:{str(cfg["node"]["port"])}'
     name = f'{str(cfg["node"]["ipAdress"])}'
-    
+    # Check passkey an return form
     if request.args.get("passkey") == None or len(request.args.get("passkey")) != 32:
         return render_template('link.html', server=server, name=name)
     else:
         passkey = str(request.args.get("passkey"))
-        catID = cfg["Categories"]["id"] + cfg["sub-Categories"]["id"]
-        catNAME = cfg["Categories"]["idLabel"] + cfg["sub-Categories"]["idLabel"]
-        data = dict()
+        catID = cfg["Categories"]["id"]
+        catNAME = cfg["Categories"]["idLabel"]
+        subID = cfg["sub-Categories"]["id"]
+        subNAME = cfg["sub-Categories"]["idLabel"]
+        catdata = dict()
+        subdata = dict()
         for index in range(len(catID)):
             key = str(catNAME[index])
             url = f"{server}/rss?id={str(catID[index])}&passkey={passkey}"
-            data[key] = url
-        return render_template('genlinks.html', data=data, server=server, name=name)
-            
-        # renderTxt = "Flux généralistes : <br>"
-        # for index in range(len(serverConfiguration["Categories"]["id"])):
-        #     renderTxt += "<strong>" + serverConfiguration["Categories"]["idLabel"][index] + "</strong><br>  " + \
-        #                  str(serverConfiguration["node"]["protocol"]) + "://" + str(serverConfiguration["node"]["ipAdress"]) +\
-        #                  ":" + str(serverConfiguration["node"]["port"]) + "/rss?id=" + str(serverConfiguration["Categories"]["id"][index])\
-        #                  + "&passkey=" + str(request.args.get("passkey")) + "<br>"
-        # renderTxt += "<br><br><br>Flux détaillés : <br>"
-        # for index in range(len(serverConfiguration["sub-Categories"]["id"])):
-        #     renderTxt += "<strong>" + serverConfiguration["sub-Categories"]["idLabel"][index] + "</strong><br>  " + \
-        #                  str(serverConfiguration["node"]["protocol"]) + "://" + str(serverConfiguration["node"]["ipAdress"]) +\
-        #                  ":" + str(serverConfiguration["node"]["port"]) + "/rss?id=" + str(serverConfiguration["sub-Categories"]["id"][index])\
-        #                  + "&passkey=" + str(request.args.get("passkey")) + "<br>"
+            catdata[key] = url
+        for index in range(len(subID)):
+            key = str(subNAME[index])
+            url = f"{server}/rss?id={str(subID[index])}&passkey={passkey}"
+            subdata[key] = url
+        return render_template('genlinks.html', catdata=catdata, subdata=subdata, server=server, name=name)
 
-        return renderTxt
+# Status route
+
 
 @app.route('/status', methods=['GET'])
 def getStatus():
     with open("config/annexes.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)        
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     server = f'{str(cfg["node"]["protocol"])}://{str(cfg["node"]["ipAdress"])}:{str(cfg["node"]["port"])}'
     name = f'{str(cfg["node"]["ipAdress"])}'
-    now = time.time()
-    renderTxt = ""
-    for index in range(len(serverConfiguration["Categories"]["id"])):
-        renderTxt += "<strong>" + serverConfiguration["Categories"]["idLabel"][index] + "</strong> : " + str(time.ctime(os.stat(os.getcwd() + "/blackhole/rss/" + str(serverConfiguration["Categories"]["id"][index]) + ".xml").st_mtime)) + "<br>"
-    renderTxt += "<br><br>"
-    for index in range(len(serverConfiguration["sub-Categories"]["id"])):
-        renderTxt += "<strong>" + serverConfiguration["sub-Categories"]["idLabel"][index] + "</strong> : " + str(time.ctime(os.stat(os.getcwd() + "/blackhole/rss/" + str(serverConfiguration["sub-Categories"]["id"][index]) + ".xml").st_mtime)) + "<br>"
+    dataCat = dict()
+    dataSub = dict()   
+    for index in range(len(cfg["Categories"]["id"])):
+        now = time.time()
+        osModTime = os.stat(f'blackhole/rss/{str(cfg["Categories"]["id"][index])}.xml').st_mtime
+        lastModTime = now - osModTime
+        val1 = time.strftime(" %d/%m/%Y --- %H:%M:%S ", time.localtime(osModTime))
+        val2 = humanize.naturaltime(lastModTime, minimum_unit="milliseconds")
+        val = (val1, val2)
+        key = cfg["Categories"]["idLabel"][index]
+        dataCat[key] = val
+    for index in range(len(cfg["sub-Categories"]["id"])):
+        now = time.time()
+        osModTime = os.stat(f'blackhole/rss/{str(cfg["sub-Categories"]["id"][index])}.xml').st_mtime
+        lastModTime = now - osModTime
+        val1 = time.strftime(" %d/%m/%Y --- %H:%M:%S ", time.localtime(osModTime))
+        val2 = humanize.naturaltime(lastModTime, minimum_unit="milliseconds")
+        val = (val1, val2)
+        key = cfg["sub-Categories"]["idLabel"][index]
+        dataSub[key] = val
+    data = {**dataCat, **dataSub}
+    return render_template('stats.html', server=server, name=name, data=data)
 
-    return renderTxt
 
 if __name__ == '__main__':
     app.run(host='localhost', debug=True, port=5000)
